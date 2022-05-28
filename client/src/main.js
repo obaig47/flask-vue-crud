d2dc76f74de648f7ff2ffe84b39fc51ba8645143
Omar Baig <o_baig@live.com>
2022-05-27 20:28:14 -0400

    Add import for bootstrap-vue.css to main.js

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
 
